b2f8df73518d54d09cd648a225708355877d9941
Sameed Zahoor <113443310+SameedZ@users.noreply.github.com>
2022-11-04 20:55:09 +0500

    Update console.js
    Co-Authored-By: Usama Fiaz <102160664+Usama-Fiaz@users.noreply.github.com>

--- console.js
+++ console.js
@@ -13,4 +13,17 @@ console.log("The application read me requires Screenshot of the dashboard or hos
 console.log("The application read me requires Screenshot of the dashboard or hosting ")
 
 
+// You can use netlify or vercel.
+
+
+
+console.log("The application read me requires Screenshot of the dashboard or hosting ")
+
+
+// You can use netlify or vercel. 
+
+
+console.log("The application read me requires Screenshot of the dashboard or hosting ")
+
+
 // You can use netlify or vercel. 
